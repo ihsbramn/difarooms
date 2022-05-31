beabc2aa1301931455efd5642c8fecb86f654de7
rizkyfadilahh <rizkyfadilah22@gmail.com>
2022-05-31 22:07:26 +0700

    homepage progress

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,31 +1,265 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container"> 
+    <div class="container-fluid">
         <div class="row justify-content-center">
-            <div class="col">
-                <h2 style="text-align: center">Welcome to Difa Rooms</h2>
-                @if (Auth::check())
-                <div class="card">
-                    <div class="card-header">{{ __('Greetings 😁') }}</div>
-    
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-                        <p>Welcome {{ Auth::user()->name }}</p>
-                        @if (Auth::user()->is_admin == 1)
-                        <a class="btn btn-light" href="{{ 'admin' }}">Menu Admin</a>
-                        @endif
-                        <a class="btn btn-light" href="{{ 'hotel' }}">Menu Hotel</a>
-                        <a class="btn btn-light" href="{{ 'forum' }}">Menu Forum</a>
+            <div class="col px-0">
+                <div class="card bg-dark text-white rounded-0">
+                    <img src="{{ URL::asset('/img/bg-beranda.png') }}" class="card-img" alt="background"
+                        height="567px">
+                    <div class="card-img-overlay d-flex">
+                        <div class="container my-auto">
+                            <h1 style="font-size: 40px; font-weight:800">Jelajahi Hidupmu, <br> Pergi Kemanapun Anda Mau
+                            </h1>
+                            <p style="font-size: 20px; font-weight:300">Kami menyediakan berbagai informasi hotel dengan
+                                segala aksesibilitasnya <br> yang akan memudahkan Anda</p>
+                            <form class="row g-2">
+                                <div class="col-6">
+                                    <input type="text" class="form-control" id="search" placeholder="Mau kemana?"
+                                        style="background-color: #D6D6D6">
+                                </div>
+                                <div class="col-2">
+                                    <button type="submit" class="btn btn-primary mb-3"
+                                        style="background-color: #244D64; border-color:#244D64">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                            class="bi bi-search" viewBox="0 0 16 16">
+                                            <path
+                                                d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
+
+                {{-- carousel hotel --}}
+                <div class="container-fluid" style="background-color: #E5F4F6">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <h2>Rekomendasi</h2>
+                            <p>Penginapan aksesibel terbaik</p>
+                        </div>
+                        <div class="col-sm-12">
+                            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <div class="row row-cols-1 row-cols-md-3 px-5 px-5">
+                                            <div class="col">
+                                                <div class="card">
+                                                    <img src="..." class="card-img-top" alt="...">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title">Card title</h5>
+                                                        <p class="card-text">This is a longer card with supporting text below as a
+                                                            natural lead-in to additional content. This content is a little bit longer.
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col">
+                                                <div class="card">
+                                                    <img src="..." class="card-img-top" alt="...">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title">Card title</h5>
+                                                        <p class="card-text">This is a longer card with supporting text below as a
+                                                            natural lead-in to additional content. This content is a little bit longer.
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col">
+                                                <div class="card">
+                                                    <img src="..." class="card-img-top" alt="...">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title">Card title</h5>
+                                                        <p class="card-text">This is a longer card with supporting text below as a
+                                                            natural lead-in to additional content.</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="carousel-item">
+                                        <div class="row row-cols-1 row-cols-md-3 px-5">
+                                            <div class="col">
+                                                <div class="card">
+                                                    <img src="..." class="card-img-top" alt="...">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title">Card title</h5>
+                                                        <p class="card-text">This is a longer card with supporting text below as a
+                                                            natural lead-in to additional content. This content is a little bit longer.
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col">
+                                                <div class="card">
+                                                    <img src="..." class="card-img-top" alt="...">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title">Card title</h5>
+                                                        <p class="card-text">This is a longer card with supporting text below as a
+                                                            natural lead-in to additional content. This content is a little bit longer.
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col">
+                                                <div class="card">
+                                                    <img src="..." class="card-img-top" alt="...">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title">Card title</h5>
+                                                        <p class="card-text">This is a longer card with supporting text below as a
+                                                            natural lead-in to additional content.</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="carousel-item">
+                                        <div class="row row-cols-1 row-cols-md-3 px-5">
+                                            <div class="col">
+                                                <div class="card">
+                                                    <img src="..." class="card-img-top" alt="...">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title">Card title</h5>
+                                                        <p class="card-text">This is a longer card with supporting text below as a
+                                                            natural lead-in to additional content. This content is a little bit longer.
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col">
+                                                <div class="card">
+                                                    <img src="..." class="card-img-top" alt="...">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title">Card title</h5>
+                                                        <p class="card-text">This is a longer card with supporting text below as a
+                                                            natural lead-in to additional content. This content is a little bit longer.
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col">
+                                                <div class="card">
+                                                    <img src="..." class="card-img-top" alt="...">
+                                                    <div class="card-body">
+                                                        <h5 class="card-title">Card title</h5>
+                                                        <p class="card-text">This is a longer card with supporting text below as a
+                                                            natural lead-in to additional content.</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"
+                                    data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon"></span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"
+                                    data-bs-slide="next">
+                                    <span class="carousel-control-next-icon"></span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {{-- carousel hotel --}}
+
+                {{-- fasilitas aksesibel --}}
+                <div class="container">
+                    <div class="row my-5">
+                        <div class="col-sm-6">
+                            <h2>Fasilitas aksesibel</h2>
+                            <p>Penginapan yang aksesibel dengan segala fasilitas <br> yang Anda butuhkan tersedia disini. Temukan <br> akomodasi yang sesuai dengan kebutuhan Anda.</p>
+                            <a class="btn btn-primary" href="" type="button">Jelajahi</a>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="row row-cols-1 row-cols-md-3 g-5">
+                                <div class="col">
+                                  <div class="card">
+                                    <img src="..." class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                      <h5 class="card-title">Card title</h5>
+                                      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div class="col">
+                                  <div class="card">
+                                    <img src="..." class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                      <h5 class="card-title">Card title</h5>
+                                      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div class="col">
+                                  <div class="card">
+                                    <img src="..." class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                      <h5 class="card-title">Card title</h5>
+                                      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div class="col">
+                                  <div class="card">
+                                    <img src="..." class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                      <h5 class="card-title">Card title</h5>
+                                      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div class="col">
+                                  <div class="card">
+                                    <img src="..." class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                      <h5 class="card-title">Card title</h5>
+                                      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                        </div>
+                    </div>
+                </div>
+                {{-- fasilitas aksesibel --}}
+
+                {{-- diskusi --}}
+                <div class="card bg-dark text-white rounded-0 border-0">
+                    <img src="{{ URL::asset('/img/bg-diskusi.png') }}" class="card-img" alt="background">
+                    <div class="card-img-overlay d-flex">
+                        <div class="container my-auto">
+                            <h2>Ingin diskusi?</h2>
+                            <a class="btn btn-primary" href="" type="button">Lanjut</a>
+                        </div>
+                    </div>
+                </div>
+                {{-- diskusi --}}
+
+                @if (Auth::check())
+                    <div class="card">
+                        <div class="card-header">{{ __('Greetings 😁') }}</div>
+
+                        <div class="card-body">
+                            @if (session('status'))
+                                <div class="alert alert-success" role="alert">
+                                    {{ session('status') }}
+                                </div>
+                            @endif
+                            <p>Welcome {{ Auth::user()->name }}</p>
+                            @if (Auth::user()->is_admin == 1)
+                                <a class="btn btn-light" href="{{ 'admin' }}">Menu Admin</a>
+                            @endif
+                            <a class="btn btn-light" href="{{ 'hotel' }}">Menu Hotel</a>
+                            <a class="btn btn-light" href="{{ 'forum' }}">Menu Forum</a>
+                        </div>
+                    </div>
             </div>
-        </div>   
+        </div>
     @else
-    @endif
-</div>
+        @endif
+    </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,10 +23,11 @@
 <body>
     @yield('up_body')
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
+        <nav class="navbar navbar-expand-md navbar-dark shadow-sm" style="background-color: #244D64; font-size: 20px">
+            <div class="container-fluid">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Difa Rooms') }}
+                    {{-- {{ config('app.name', 'Difa Rooms') }} --}}
+                    <img src="{{URL::asset('/img/difarooms-logo.png')}}" alt="">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -37,6 +38,25 @@
                     <ul class="navbar-nav me-auto">
 
                     </ul>
+                    
+                    <!-- Center Of Navbar -->
+                    <ul class="navbar-nav mx-auto">
+                        <li class="nav-item pe-5">
+                            <a class="nav-link" href="{{ url('/') }}">{{ __('Beranda') }}</a>
+                        </li>
+
+                        <li class="nav-item px-5">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Hotel') }}</a>
+                        </li>
+
+                        <li class="nav-item px-5">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Forum') }}</a>
+                        </li>
+
+                        <li class="nav-item ps-5">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Tentang Kami') }}</a>
+                        </li>
+                    </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
@@ -44,13 +64,13 @@
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link btn btn-outline-light rounded-pill px-4 me-2" type="button" href="{{ route('login') }}" style="font-size: 20px">{{ __('Login') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link btn btn-light rounded-pill px-3 ms-2" type="button" href="{{ route('register') }}" style="color: #244D64; font-size: 20px">{{ __('Register') }}</a>
                                 </li>
                             @endif
                         @else
@@ -81,10 +101,30 @@
             </div>
         </nav>
 
-        <main class="py-4">
+        <main>
             @yield('content')
         </main>
     </div>
     @yield('lower_body')
+    <!-- Footer -->
+    <footer>
+        <div class="container-fluid" style="background-color: #244D64">
+            <div class="row">
+                <div class="col-4 m-5">
+                    <img src="{{URL::asset('/img/difarooms-logo.png')}}" alt="">
+                    <p class="mt-2" style="font-weight: 300; font-size: 14px; color: #FFFFFF">Platform berbagi informasi <br>
+                        mengenai aksesibilitas bagi <br>
+                        penyandang disabilitas</p>
+                </div>
+                <div class="col-4 d-flex mx-auto">
+                    <div class="row my-auto" style="font-size: 16px; font-weight:300; color: #FFFFFF">
+                            <p>Tujuan</p>
+                            <p>Pusat Bantuan</p>
+                            <p>Kontak</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
