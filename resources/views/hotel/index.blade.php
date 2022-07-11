b6df2dd2fe4d7f036c8bad93cc8d4068cc59c329
ihsbramn <abdurahmanihsan25@gmail.com>
2022-07-11 19:12:38 +0700

    update fav

--- resources/views/hotel/index.blade.php
+++ resources/views/hotel/index.blade.php
@@ -351,8 +351,14 @@
                                             </div>
                                             <div class="row">
                                                 <div class="col-6">
+
                                                 </div>
                                                 <div class="col-6">
+                                                    @foreach ($fav as $fv)
+                                                    @if ($fv->fv_hotel_id == $htl->id)
+                                                        <p>{{ $fv->fv_hotel_name }}</p>
+                                                    @endif
+                                                    @endforeach
                                                 </div>
                                             </div>
                                             <div class="row">
@@ -440,7 +446,7 @@
                                             <td>
                                                 <a class="btn btn-primary"
                                                     href="{{ route('/hotel/show', $data->id) }}">Show</a>
-                                                {{-- <form action="{{ route('/favourites/store') }}" method="POST">
+                                                <form action="{{ route('/favourites/store') }}" method="POST">
                                         @csrf
                                         <div class="form-group">
                                             <input type="text" class="form-control" id="fv_user_id" name="fv_user_id" value="{{ Auth::user()->id }}" hidden>
@@ -450,7 +456,7 @@
                                         </div>
                                         <br>
                                         <button href="submit" class="btn btn-dark">Tambah ke Favorit</button>
-                                        </form> --}}
+                                        </form>
                                             </td>
                                         </tr>
                                     </tbody>
