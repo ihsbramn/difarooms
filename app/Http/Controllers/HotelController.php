38663d054bb8ed67d8763d6db83d083ee834ce3f
ihsbramn <abdurahmanihsan25@gmail.com>
2022-07-19 15:46:11 +0700

    facility exist conditioning

--- app/Http/Controllers/HotelController.php
+++ app/Http/Controllers/HotelController.php
@@ -316,11 +316,13 @@ class HotelController extends Controller
         
         $hotel = Hotel::find($id);
         $hotel_img = Hotel_Img::where('ht_id', '=' , $id)->get();
+        $hotel_fas = HotelFascility::where('ht_id', '=' , $id)->get();
 
-        // dd($hotel,$hotel_img);
+        // dd($hotel,$hotel_img,$hotel_fas);
         return view('/hotel/edit', compact(
             'hotel',
-            'hotel_img'
+            'hotel_img',
+            'hotel_fas'
         ));
     }
 
