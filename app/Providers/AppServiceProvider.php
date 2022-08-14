b60e823fa4cb4cee6035bfc4f0962a79ea5d6481
ihsbramn <abdurahmanihsan25@gmail.com>
2022-08-14 17:21:44 +0700

    init hostinger

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -33,7 +33,7 @@ class AppServiceProvider extends ServiceProvider
         });
         Schema::defaultStringLength(191);
         if ($this->app->environment('production')) {
-            URL::forceScheme('https');
+            URL::forceScheme('http');
         }
     }
 }
