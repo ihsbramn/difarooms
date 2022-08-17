b6317840ed662cba88f1078b41e87635f240cc6b
ihsbramn <abdurahmanihsan25@gmail.com>
2022-08-17 19:23:17 +0700

    update

--- config/mail.php
+++ config/mail.php
@@ -96,6 +96,14 @@ return [
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
+    'stream' => [
+        'ssl' => [
+            'allow_self_signed' => true,
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+        ],
+    ],
+
     /*
     |--------------------------------------------------------------------------
     | Markdown Mail Settings
